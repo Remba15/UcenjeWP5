1a2ef7086dd8d052ef376cec276aa1882c5fffc3
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-05-25 11:48:25 +0200

    Rješenje Projekti ERA dijagrama

---
+++ Vježba za subotu 25.05.2024/IspitERA/SQLQueryProjekti.sql
@@ -0,0 +1,35 @@
+--create database Projekti;
+
+--use Projekti;
+
+--create table projekt(
+--	sifra INT NOT NULL,
+--	naziv VARCHAR(45) NOT NULL,
+--	datumpocetka DATETIME,
+--	datumkraja DATETIME,
+--	PRIMARY KEY(sifra)
+--);
+
+--create table odjel(
+--	sifra INT NOT NULL,
+--	naziv VARCHAR(45),
+--	PRIMARY KEY(sifra)
+--);
+
+--create table zaposlenik(
+--	sifra INT NOT NULL PRIMARY KEY,
+--	nadredeni INT NOT NULL,
+--	ime VARCHAR(45),
+--	prezime VARCHAR(45),
+--	datumrodenja DATETIME,
+--	placa DECIMAL(18,2),
+--	odjel INT FOREIGN KEY REFERENCES odjel(sifra)
+--);
+
+
+
+--create table sudjeluje(
+--	zaposlenik INT NOT NULL FOREIGN KEY REFERENCES zaposlenik(sifra),
+--	projekt INT NOT NULL FOREIGN KEY REFERENCES projekt(sifra),
+--	datumpristupanja DATETIME
+--);
