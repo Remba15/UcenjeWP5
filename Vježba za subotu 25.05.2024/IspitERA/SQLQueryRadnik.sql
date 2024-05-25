91c282c7273c049b8216e98a629ee93f2345becc
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-05-25 11:59:14 +0200

    Rješenje Radnik ERA dijagrama

---
+++ Vježba za subotu 25.05.2024/IspitERA/SQLQueryRadnik.sql
@@ -0,0 +1,10 @@
+--create database radnik;
+
+--use radnik;
+
+--create table radnik(
+--	sifra INT NOT NULL PRIMARY KEY,
+--	ime NVARCHAR(50) NOT NULL,
+--	prezime NVARCHAR(50) NOT NULL,
+--	nadredeni INT NOT NULL FOREIGN KEY REFERENCES radnik(sifra)
+--);
