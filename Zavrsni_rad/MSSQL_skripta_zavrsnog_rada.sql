5a6c90ab7d5f8edb0962500835daee95bc788486
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-06-06 13:16:34 +0200

    Dodavanje MSSQL skripte za treću verziju završnog rada

--- Zavrsni_rad/MSSQL_skripta_zavrsnog_rada.sql
+++ Zavrsni_rad/MSSQL_skripta_zavrsnog_rada.sql
@@ -2,36 +2,33 @@
 
 --use EdunovaWebProgramiranjeZavrsniRad;
 
---create table Player(
---	Player_ID int not null primary key identity(1,1),
---	Username varchar(50) not null,
---	RegistrationDate date not null,
---	Region varchar(50) not null
---);
+create table Player(
+	ID int not null primary key identity(1,1),
+	Username varchar(50) not null,
+	RegistrationDate date not null,
+	Region varchar(50) not null
+);
 
---create table Game(
---	Game_ID int not null primary key identity(1,1),
---	Title varchar(50) not null,
---	Developer varchar(50),
---	GamePlatform varchar(50),
---	GameDescription text,
---	CoverImage image
---);
+create table Game(
+	ID int not null primary key identity(1,1),
+	Title varchar(50) not null,
+	Developer varchar(50),
+	GamePlatform varchar(50),
+	GameDescription text,
+	CoverImage image
+);
 
---create table Trophy(
---	Trophy_ID int not null primary key identity(1,1),
---	Title varchar(50) not null,
---	TrophyDescription text,
---	Game_ID int not null foreign key references Game(Game_ID),
---	TrophyType varchar(10),
---	TrophyIcon image,
---	DateAchieved datetime
---);
+create table Trophy(
+	ID int not null primary key identity(1,1),
+	Title varchar(50) not null,
+	TrophyDescription text,
+	Game_ID int not null foreign key references Game(ID),
+	TrophyType varchar(10),
+	TrophyIcon image,
+);
 
---create table Comparison(
---	Player1_ID int not null foreign key references Player(Player_ID),
---	Player2_ID int not null foreign key references Player(Player_ID),
---	Game_ID int not null foreign key references Game(Game_ID),
---	Trophy_ID int not null foreign key references Trophy(Trophy_ID),
---	Achieved bit
---);
+create table Achieve(
+	Player_ID int not null foreign key references Player(ID),
+	Trophy_ID int not null foreign key references Trophy(ID),
+	DateAchieved DATETIME
+);
