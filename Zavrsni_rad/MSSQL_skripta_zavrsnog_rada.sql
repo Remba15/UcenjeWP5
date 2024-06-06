b352cf7baf1085fc8ca27c230ab90429506cc58e
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-06-06 15:19:17 +0200

    Dodavanje select i insert naredbi u MSSQL skriptu

--- Zavrsni_rad/MSSQL_skripta_zavrsnog_rada.sql
+++ Zavrsni_rad/MSSQL_skripta_zavrsnog_rada.sql
@@ -32,3 +32,24 @@ create table Achieve(
 	Trophy_ID int not null foreign key references Trophy(ID),
 	DateAchieved DATETIME
 );
+
+
+select * from Player;
+
+insert into Player(Username, RegistrationDate, Region) values
+	('Remba15', '2023-05-13', 'Croatia'), ('User2', '2024-06-06', 'France'), ('User3', '2020-05-12', 'Japan');
+
+select * from Game;
+
+insert into Game(Title, Developer, GamePlatform) values
+	('Dead Space 2', 'Visceral Games', 'Playstation 3'), ('Resident evil', 'Capcom', 'Playstation'), ('Lethal Company', 'Zeekersrblx', 'Steam')
+
+select * from Trophy
+
+insert into Trophy (Title, Game_ID, TrophyType) values
+	('The Final Frontier', 1, 'Platinum'), ('Enter Survival', 2, 'Bronze'), ('Is This A Jetpack?', 3, 'Silver');
+
+select * from Achieve;
+
+insert into Achieve (Player_ID, Trophy_ID, DateAchieved) values
+	(1, 2, '2021-04-05 18:23'), (1, 3, '2023-02-02 0:18'), (2, 1, '2015-06-13 10:25');
