c349ef25b555b2661eccbb34e6e80c414c53036a
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-06-07 11:41:25 +0200

    Rješenje zadatka 1

---
+++ Zadaci za vježbu/06.06.2024/Z1/SQLQueryZ1-060624.sql
@@ -0,0 +1,21 @@
+﻿use master
+go
+drop database if exists Opcine
+go
+create database Opcine
+go
+use Opcine;
+
+create table Nacelnici(
+	sifra int not null primary key identity(1,1),
+	ime varchar(30) not null,
+	prezime varchar(30) not null,
+	datumRodenja date
+);
+
+insert into Nacelnici (ime, prezime, datumRodenja) values
+	('Marin', 'Marić', '1985-05-12'), ('Ivan', 'Ivić', '1992-01-05'), ('Vinko', 'Vinić', '1968-06-25')
+
+select * from Nacelnici;
+
+update Nacelnici set ime='Tomislav' where sifra=3;
