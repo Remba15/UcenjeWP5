96ac1e8bb7fa632bb05a72c0ca2a3032688f818f
Remba15 <renatokuna01@gmail.com>
2024-06-09 16:13:01 +0200

    Dodavanje rjesenja zadatka za vjezbu 3

---
+++ Zadaci za vježbu/06.06.2024/Z3/SQLQueryZ3-060624.sql
@@ -0,0 +1,36 @@
+use master
+go
+drop database if exists Restoran
+go
+create database Restoran
+go
+use Restoran
+
+create table Persons(
+    ID int not null primary key identity(1,1),
+    LastName varchar(50) not null,
+    FirstName varchar(50) not null,
+    Age int
+);
+
+create table Orders(
+    OrderID int not null primary key identity(1,1),
+    OrderNumber varchar(20),
+    PersonID int foreign key references Persons(ID)
+);
+
+insert into Persons(LastName, FirstName, Age) values
+    ('Kuna', 'Renato', 28), ('Marinković', 'Marin', 25), ('Ivanović', 'Ivana', 45);
+
+select * from Persons;
+
+insert into Orders(OrderNumber, PersonID) values
+    ('56486487', 2), ('00154687', 1), ('549753164', 3)
+
+select * from Orders;
+
+update Persons LastName='Kunović' where LastName='Kuna';
+
+update Orders OrderNumber='56486552' where PersonID=1;
+
+delete from Orders where OrderNumber='549753164';
