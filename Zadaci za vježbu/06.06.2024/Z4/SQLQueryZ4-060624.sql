2b0befd56e104b4d525fd2d7e39a0024d511833d
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-06-10 12:14:31 +0200

    Dodavanje rjesenja cetvrtog zadatka za vjezbu

---
+++ Zadaci za vježbu/06.06.2024/Z4/SQLQueryZ4-060624.sql
@@ -0,0 +1,21 @@
+﻿use master
+go
+drop database if exists Samostan
+go
+create database Samostan
+go
+use Samostan;
+
+create table Svecenici(
+	sifra int not null primary key identity(1,1),
+	ime varchar(50),
+	prezime varchar(50),
+	nadredeni int foreign key references Svecenici(sifra)
+);
+
+insert into Svecenici (ime, prezime, nadredeni) values
+	('Renato', 'Kuna', null), ('Ivan', 'Ivanović', 1), ('Marko', 'Markić', 1);
+
+select * from Svecenici;
+
+update Svecenici set nadredeni=2 where ime='Marko' AND prezime='Markić';
