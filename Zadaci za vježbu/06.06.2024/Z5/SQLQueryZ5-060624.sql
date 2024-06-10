91cc30aba1ce7742c33d11dd5d72420ff767ac7f
Remba15 <renatokuna01@gmail.com>
2024-06-10 14:00:03 +0200

    Nadopunjavanje petog zadatka za vjezbu

--- Zadaci za vježbu/06.06.2024/Z5/SQLQueryZ5-060624.sql
+++ Zadaci za vježbu/06.06.2024/Z5/SQLQueryZ5-060624.sql
@@ -30,3 +30,33 @@ create table Mjesto(
 	opcina int not null foreign key references Opcina(sifra),
 	naziv varchar(50) not null
 );
+
+select * from Zupan;
+
+insert into Zupan (ime, prezime) values
+	('Renato', 'Kuna'), ('Marko', 'Markić'), ('Ivana', 'Ivić');
+
+select * from Zupanija;
+
+insert into Zupanija (naziv, zupan) values
+	('Osjecko-Baranjska', 1), ('Vukovarsko-Srijemska', 3), ('Splitsko-Dalmatinska', 2);
+
+select * from Opcina;
+
+insert into Opcina (zupanija, naziv) values
+	(1, 'Grad Osijek'), (1, 'Beli Manastir'), (3, 'Grad Split'), (3, 'Dugopolje'), (2, 'Grad Vukovar'), (2, 'Ilok');
+
+select * from Mjesto;
+
+insert into Mjesto (opcina, naziv) values
+	(1, 'Antunovac'), (1, 'Josipovac'), (2, 'Bilje'), (2, 'Darda'), (3, 'Split1'), (3, 'Split2')
+	(4, 'Dugopolje1'), (4, 'Dugopolje2'), (5, 'Vukovar1'), (5, 'Vukovar2'), (6, 'Ilok1'), (6, 'Ilok2');
+
+update Mjesto set naziv='Vukovar3' where naziv='Vukovar1';
+update Mjesto set naziv='Dugopolje4' where naziv='Dugopolje2';
+update Mjesto set naziv='Nemetin' where naziv='Josipovac';
+update Mjesto set naziv='Ilok5' where naziv='Ilok2';
+update Mjesto set naziv='Split8' where naziv='Split1';
+
+delete from Mjesto where opcina=2 AND opcina=4;
+delete from Opcina where sifra=2 AND sifra=4;
