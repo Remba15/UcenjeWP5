6b3a1c0a8a5e8971c16f7e436854a2c33bad90f6
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-06-10 12:22:44 +0200

    Dodavanje rjesenja petog zadatka za vjezbu

---
+++ Zadaci za vježbu/06.06.2024/Z5/SQLQueryZ5-060624.sql
@@ -0,0 +1,32 @@
+﻿use master
+go
+drop database if exists Zupanija
+go
+create database Zupanija
+go
+
+use Zupanija
+
+create table Zupan(
+	sifra int not null primary key identity(1,1),
+	ime varchar(50) not null,
+	prezime varchar(50) not null
+);
+
+create table Zupanija(
+	sifra int not null primary key identity(1,1),
+	naziv varchar(50) not null,
+	zupan int not null foreign key references Zupan(sifra)
+);
+
+create table Opcina(
+	sifra int not null primary key identity(1,1),
+	zupanija int not null foreign key references Zupanija(sifra),
+	naziv varchar(50) not null
+);
+
+create table Mjesto(
+	sifra int not null primary key identity(1,1),
+	opcina int not null foreign key references Opcina(sifra),
+	naziv varchar(50) not null
+);
