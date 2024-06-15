d96e2642eda81ae6fc236607debe22e16a970388
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-06-15 13:05:02 +0200

    Manji ispravci na skripti MSSQL

--- Zavrsni_rad/MSSQL_skripta_zavrsnog_rada.sql
+++ Zavrsni_rad/MSSQL_skripta_zavrsnog_rada.sql
@@ -37,23 +37,22 @@ create table Achieve(
 	DateAchieved DATETIME
 );
 
-
-select * from Player;
-
 insert into Player(Username, RegistrationDate, Region) values
 	('Remba15', '2023-05-13', 'Croatia'), ('User2', '2024-06-06', 'France'), ('User3', '2020-05-12', 'Japan');
-
-select * from Game;
+	
+select * from Player;
 
 insert into Game(Title, Developer, GamePlatform) values
-	('Dead Space 2', 'Visceral Games', 'Playstation 3'), ('Resident evil', 'Capcom', 'Playstation'), ('Lethal Company', 'Zeekersrblx', 'Steam')
-
-select * from Trophy
+	('Dead Space 2', 'Visceral Games', 'Playstation 3'), ('Resident evil', 'Capcom', 'Playstation'), ('Lethal Company', 'Zeekersrblx', 'Steam');
+	
+select * from Game;
 
 insert into Trophy (Title, Game_ID, TrophyType) values
 	('The Final Frontier', 1, 'Platinum'), ('Enter Survival', 2, 'Bronze'), ('Is This A Jetpack?', 3, 'Silver');
-
-select * from Achieve;
+	
+select * from Trophy
 
 insert into Achieve (Player_ID, Trophy_ID, DateAchieved) values
 	(1, 2, '2021-04-05 18:23'), (1, 3, '2023-02-02 0:18'), (2, 1, '2015-06-13 10:25');
+	
+select * from Achieve;
