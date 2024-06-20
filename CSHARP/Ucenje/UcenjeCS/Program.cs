cd8eae4db7b71da1a6be5a3ae69b9b6273cb8192
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-06-20 21:00:09 +0200

    Testiranje kratica

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Edunova!");
+Console.WriteLine("Osijek");
+
+//Ovo je komentar jedna linija
+
+/* 
+   Komentar kroz više linija
+*/
+
+Console.WriteLine("zadnja linija čćčđščćč");
