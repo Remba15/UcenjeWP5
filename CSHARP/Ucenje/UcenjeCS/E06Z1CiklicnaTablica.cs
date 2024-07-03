2c6c9886c2a0278ffedd640eadc238125ec1eb66
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-07-03 13:21:46 +0200

    Dodavanje zadatka za vježbanje E06Z1

---
+++ CSHARP/Ucenje/UcenjeCS/E06Z1CiklicnaTablica.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E06Z1CiklicnaTablica
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("Unesite broj redaka tablice: ");
+            int brojRedaka = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite broj stupaca tablice: ");
+            int brojStupaca = int.Parse(Console.ReadLine());
+
+
+
+            int[,] tablica = new int[brojRedaka, brojStupaca];
+
+            int umnozak = brojRedaka * brojStupaca;
+
+            int korak = 1;
+            int padajuciRedak = brojRedaka - 1,
+                rastuciRedak = 0,
+                padajuciStupac = brojStupaca - 1,
+                rastuciStupac = 0;
+
+
+            while(korak < umnozak)
+            {
+                for (int i = padajuciRedak; ;)
+                {
+                    for (int j = padajuciStupac; j >= rastuciStupac; j--)
+                    {
+                        tablica[i, j] = korak++;
+                    }
+                    --padajuciRedak;
+                    break;
+                }
+
+                for (int j = rastuciStupac; ;)
+                {
+                    for (int i = padajuciRedak; i >= rastuciRedak; i--)
+                    {
+                        tablica[i, j] = korak++;
+                    }
+                    ++rastuciStupac;
+                    break;
+                }
+
+                for (int i = rastuciRedak; ;)
+                {
+                    for (int j = rastuciStupac; j <= padajuciStupac; j++)
+                    {
+                        tablica[i, j] = korak++;
+                    }
+                    ++rastuciRedak;
+                    break;
+                }
+
+                for (int j = padajuciStupac; ;)
+                {
+                    for (int i = padajuciRedak; i <= rastuciRedak; i++)
+                    {
+                        tablica[i, j] = korak++;
+                    }
+                    --padajuciStupac;
+                    break;
+                }
+            }
+
+
+
+            for (int i = 0; i < tablica.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablica.GetLength(1); j++)
+                {
+                    Console.Write(String.Format("{0, 10}", tablica[i, j] + " "));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -8,13 +8,31 @@ Console.WriteLine("zadnja linija čćčđščćč");
 
 using UcenjeCS;
 
-E06ForPetlja.Izvedi();
+#region E06
+E06Z1CiklicnaTablica.Izvedi();
+//E06ForPetlja.Izvedi();
+#endregion
+
+#region E05
 //E05Nizovi.Izvedi();
+#endregion
+
+#region E04
 //E04Z1.Izvedi();
 //E04UvjetnoGrananjeSwitch.Izvedi();
+#endregion
+
+#region E03
 //E03Z3.Izvedi();
 //E03Z2.Izvedi();
 //E03Z1.Izvedi();
 //E03UvjetnoGrananjeIf.Izvedi();
+#endregion
+
+#region E02
 //E02VarijableTipoviPodataka.Izvedi();
+#endregion
+
+#region E01
 //E01UlazIzlaz.Izvedi();
+#endregion
