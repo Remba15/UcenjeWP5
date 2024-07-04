d25e8da4516c511efad763ace9d58f23d783ac59
nikolamilic1 <renatokuna01@gmail.com>
2024-07-04 21:04:00 +0200

    Dodavanje E07Z2

---
+++ CSHARP/Ucenje/UcenjeCS/E07Z2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E07Z2
+    {
+        internal static void Izvedi()
+        {
+            //Program od korisnika unosi 10 brojeva
+            //Program ispisuje prvo zbroj svih unesenih brojeva
+            //i nakon toga ispisuje unesene brojeve jedno ispod drugog
+
+            Console.WriteLine("Unesite 10 brojeva: ");
+            int[] nizBrojeva = new int[10];
+            int zbroj = 0;
+
+
+            for (int i = 0; i < nizBrojeva.Length; i++)
+            {
+                Console.WriteLine("Unesite vrijednost {0}. broja: ", i + 1);
+                try
+                {
+                    nizBrojeva[i] = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Niste unijeli broj!");
+                }
+                zbroj += nizBrojeva[i];
+            }
+            Console.WriteLine(zbroj);
+            foreach (var b in nizBrojeva)
+            {
+                Console.WriteLine(b);
+            }
+
+        }
+    }
+}
