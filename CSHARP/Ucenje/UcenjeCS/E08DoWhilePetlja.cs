9456154374e4d4fa2865f1b6c8a7d5fd1d28eb27
nikolamilic1 <renatokuna01@gmail.com>
2024-07-04 20:13:18 +0200

    Dodavanje E08DoWhilePetlja

---
+++ CSHARP/Ucenje/UcenjeCS/E08DoWhilePetlja.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E08DoWhilePetlja
+    {
+        public static void Izvedi()
+        {
+
+            do{
+
+                Console.WriteLine("Edunova");
+
+            } while (false);
+
+            while(false) Console.WriteLine("Osijek");
+
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i++);
+            } while (i < 10);
+
+
+
+        }
+    }
+}
