db3df27c611029922a8949e9f941e8c30fb9bfef
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-07-09 20:59:36 +0200

    Dodavanje zadatka E11Z1

---
+++ CSHARP/Ucenje/UcenjeCS/E11Z1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E11Z1
+    {
+        internal static void Izvedi()
+        {
+            //Program ucitava 2 broja i ispisuje zbroj
+            Console.WriteLine(
+                E11Metode.UcitajCijeliBroj("Unesi prvi broj", int.MinValue, int.MaxValue) + 
+                E11Metode.UcitajCijeliBroj("Unesi drugi broj", int.MinValue, int.MaxValue)
+                );
+
+
+        }
+    }
+}
