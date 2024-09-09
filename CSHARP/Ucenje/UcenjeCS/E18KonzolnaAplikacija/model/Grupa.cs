e6401191d6dc4f8e38e48dcf8b4d5c3bd495de26
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-09-09 17:20:33 +0200

    Dodavanje metode za unos sifre i naziva grupe, ToString() override.

--- CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/model/Grupa.cs
+++ CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/model/Grupa.cs
@@ -1,12 +1,24 @@
 ﻿namespace UcenjeCS.E18KonzolnaAplikacija.Model
 {
-    internal class Grupa:Entitet
+    internal class Grupa : Entitet
     {
         public string? Naziv { get; set; }
         public Smjer? Smjer { get; set; }
         public string? Predavac { get; set; }
         public int? MaksimalnoPolaznika { get; set; }
         public List<Polaznik>? Polaznici { get; set; }
+
+        public override string ToString()
+        {
+            return this.Naziv + " (" + this.Smjer?.Naziv + "), " + this.Polaznici?.Count + " polaznika";
+        }
+
+        public void UnosSifreNaziva()
+        {
+            this.Sifra = Pomocno.UcitajRasponBroja("Unesi šifru grupe", 1, int.MaxValue);
+            this.Naziv = Pomocno.UcitajString("Unesi naziv grupe", 50, true);
+        }
+
 
     }
 }
